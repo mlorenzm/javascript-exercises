bc97508433add02fdd0ef3ce40072526d780ab01
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-19 19:39:53 +0000

    Add solutions to exercises 1,2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    result = '';
+    if (n < 0) {result = 'ERROR'
+    }
+     else{
+        for (let i = 0; i < n; i++) {
+            result += str;
+        } 
+    }   
+        return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let array = str.split('');
+    let result = array.reverse().join('');
+    return result;
 };
 
 // Do not edit below this line
