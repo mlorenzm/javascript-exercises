60f09e9e26f099fec4da65fc75d72ed0670d72bf
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-23 19:30:06 +0000

    Add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(n,m) {
+    if (typeof n != 'number' || typeof m != 'number'){
+        return 'ERROR';
+    } else if (n < 0 || m < 0){
+        return 'ERROR';
 
+    } else{
+        let largeNum = Math.max(n,m);
+        let smallNum = Math.min(n,m);
+        let result = 0;
+        for (i = smallNum; i <= largeNum; i++){
+            result = result + i;
+            }
+        return result;
+    }
 };
 
 // Do not edit below this line
