6db8fcf6edd9962e0d7f4219a318e11b6061466e
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-23 20:17:00 +0000

    Add solution to 06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  let result = (n - 32) * 0.5556
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  let result = n * 1.8 + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
