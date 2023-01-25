60a7c97758ba035ed25ce8d526f48992e2b11333
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-25 21:01:16 +0000

    Add solution to 08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let string = input.toLowerCase();
+     string = string.replace(/[^\w\s\']|_/g, "")
+         .replace(/\s+/g, ""); // Thanks StackOverflow
+    console.log(string);
+    let forward = string.split('');
+    console.log(forward)
+    let reverse = forward.reverse();
+    console.log(reverse)
+    let joined = reverse.join('');
+    console.log(joined)
+    if (string == joined){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
