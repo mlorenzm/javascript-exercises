362f6097de0050e579d95138db373dafbd038311
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-27 19:23:02 +0000

    Add solution to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    for (let i = 0; i < array.length; i++){
+        if (array[i].yearOfDeath == undefined){
+            array[i].yearOfDeath = '2023';
+        } 
+    array[i].age = array[i].yearOfDeath - array[i].yearOfBirth;
+    };
+    const highest = array.reduce((previous, current) => {
+        return current.age > previous.age ? current : previous;
+      }); return highest; // Thanks StackOverflow
 };
 
 // Do not edit below this line
