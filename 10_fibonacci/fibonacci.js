0a83c367a8d5b63bb5fbcddbab05bc2072eba31d
mlorenzm <mlorenzomohamed@gmail.com>
2023-01-27 18:35:32 +0000

    Add solution to 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n <= 0){
+        return 'OOPS';
+    } else{
+    fib = [1, 1];
+    for (let i = 2; i < n; i++){
+        fib.push(fib[i-1] + fib[i-2]);
+    } return fib.pop();
+    }
 };
 
 // Do not edit below this line
